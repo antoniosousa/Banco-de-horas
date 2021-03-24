0b259240205b0c7d38d9ec963408f38212b78a86
Antonio Sousa <antonioufpi@gmail.com>
2021-03-24 10:57:24 -0300

    inserido comentários para distinguir o que seria apps terceiros e apps da aplicação

--- bancodehoras/bancodehoras/settings.py
+++ bancodehoras/bancodehoras/settings.py
@@ -12,13 +12,16 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    # third
     'grappelli',
+    # default
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
     'usuario',
     'core',
     'movimentacao',
